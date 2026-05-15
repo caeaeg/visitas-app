12a4558d242de8fc8aa407bf5cb7f90fee2c6b0f
Arturo Ezequiel Gonzalez <112989145+caeaeg@users.noreply.github.com>
2026-05-15 13:09:05 -0300

    Update Department.js

--- models/Department.js
+++ models/Department.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const departmentSchema = new mongoose.Schema({
-  number: String,
-  buildingId: mongoose.Schema.Types.ObjectId
+  number: String, // Ejemplo: "1A", "402"
+  buildingId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Building", // Conexión oficial con el modelo Building
+    required: true 
+  }
 });
 
 module.exports = mongoose.model("Department", departmentSchema);
